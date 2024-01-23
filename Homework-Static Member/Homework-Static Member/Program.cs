de5e9b1d18b61091016282c8d34b1bcb80f20fb3
unknown <farid12344321>
2024-01-23 03:01:44 -0800

    homework

---
+++ Homework-Static Member/Homework-Static Member/Program.cs
@@ -0,0 +1,36 @@
+﻿using Homework_Static_Member;
+
+
+string fullName;
+string groupNo;
+do
+{
+    Console.WriteLine("FullName daxil edin");
+    fullName = Console.ReadLine();
+} while (!Student.CheckFullName(fullName));
+do
+{
+    Console.WriteLine("GroupNo daxil edin");
+    groupNo = Console.ReadLine();
+} while (!Student.CheckGroupNo(groupNo));
+Student student = new Student()
+{
+    FullName = fullName,
+    GroupNo = groupNo
+};
+Console.WriteLine($"FullName: {student.FullName} - GroupNo: {student.GroupNo}");
+
+
+
+
+
+
+
+
+
+
+
+
+//string text = "Farid Abdullayev";
+//var text2 = text.Substring(0,text.IndexOf(" ")+);
+//Console.WriteLine(text2);
---
+++ Homework-Static Member/Homework-Static Member/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Homework_Static_Member
+{
+    internal class Student
+    {
+        private string _fullName;
+        private string _groupNo;
+        public int Age;
+        public string GroupNo 
+        {
+            get
+            {
+                return _groupNo;
+            }
+            set
+            {
+                if(CheckGroupNo(value))
+                {
+                    _groupNo = value;
+                }
+            }
+        }
+        public string FullName 
+        {
+            get
+            {
+                return _fullName;
+            }
+
+            set
+            {
+                if (CheckFullName(value)) 
+                {
+                    _fullName = value;
+                }
+            }
+        }
+        public static bool CheckGroupNo(string groupNo)
+        {
+            if (groupNo.Length != 4) return false;
+            if (string.IsNullOrWhiteSpace(groupNo)) return false;
+            if (!char.IsLetter(groupNo[0])) return false;
+            if (!char.IsUpper(groupNo[0])) return false;
+            for (int i = 1; i < groupNo.Length; i++)
+            {
+                if (!char.IsNumber(groupNo[i])) return false;
+            }
+            return true;
+        }
+        public static bool CheckFullName(string fullName)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))  return false;
+            var text1 = fullName.Trim();
+            var text2 = text1.Substring(text1.IndexOf(" ") + 1);
+            if (!text1.Contains(" ")) return false;
+            if (!char.IsUpper(text1[0])) return false;
+            if (!char.IsUpper(text2[0])) return false;
+
+            return true;
+        }
+    }
+}
